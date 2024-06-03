d960112528495533885902230196eaaddf66c0b1
Kikayoung <135994708+Kikayoung@users.noreply.github.com>
2024-06-03 14:14:09 +0900

    commit

--- sql.sql
+++ sql.sql
@@ -1,17 +1,6 @@
 CREATE DATABASE godiva;
 USE godiva;
 
-CREATE TABLE account (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    userid VARCHAR(255) NOT NULL,
-    userpw VARCHAR(255) NOT NULL,
-    userphonenumber VARCHAR(255) NOT NULL,
-    useremail VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-DROP TABLE user;
-FLUSH PRIVILEGES;
-
 CREATE TABLE inquiries (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -28,24 +17,54 @@ CREATE TABLE inquiries (
 SELECT * FROM inquiries;
 SHOW TABLES;
 
--- 상품 목록 테이블
-CREATE TABLE products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
+ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';
+
+
+
+USE godiva;
+#DROP TABLE account;
+#nodejsproject
+CREATE TABLE account (
+    userid VARCHAR(30) NOT NULL primary key,
     name VARCHAR(100) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
+    userpw VARCHAR(64) NOT NULL,
+    userphonenumber VARCHAR(20) NOT NULL,
+    useremail VARCHAR(50) NOT NULL,
+    privacy_agreement BOOLEAN NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- 장바구니 테이블
-CREATE TABLE cart (
-    cart_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+CREATE TABLE inquiries (
+    in_id INT AUTO_INCREMENT PRIMARY KEY,
+    userid VARCHAR(30) NOT NULL,
+    inquiry_category VARCHAR(50) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    file_attachment VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userid) REFERENCES account(userid)
+);
+SELECT * FROM inquiries;
+SELECT * FROM inquiries WHERE in_id = 1;
+
+SHOW TABLES;
+
+create table product(
+pid int not null auto_increment primary key,
+pname varchar(50) not null,
+pprice int not null
 );
 
+SELECT * FROM product;
+
+DROP TABLE product;
+desc product;
+
+insert into product(pname,pprice) values ("골드리커버리 6개입",32000);
+insert into product(pname,pprice) values ("골드리커버리 9개입",42000);
+insert into product(pname,pprice) values ("골드리커버리 15개입",65000);
+insert into product(pname,pprice) values ("하트오브골드 8개입",34000);
+insert into product(pname,pprice) values ("골드리커버리 12개입",52000);
+insert into product(pname,pprice) values ("까레 밀크 어쏘트먼트 16개입",34000);
+insert into product(pname,pprice) values ("까레 어쏘트먼트 밀크&다크 24조각",50000);
+insert into product(pname,pprice) values ("까레 어쏘트먼트 50% 16개입",34000);
